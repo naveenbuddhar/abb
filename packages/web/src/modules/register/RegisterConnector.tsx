ad0404beff9f0ec2a40080721b5ac651f376ac10
Kumar <Naveen.Kumar@acer.com>
2018-08-16 09:18:03 +0530

    Included CodeGen

--- packages/web/src/modules/register/RegisterConnector.tsx
+++ packages/web/src/modules/register/RegisterConnector.tsx
@@ -3,12 +3,11 @@ import { RegisterController } from "@abb/controller";
 import { RegisterView } from "./ui/RegisterView";
 
 export class RegisterConnector extends React.PureComponent {
-
   render() {
     return (
-        <RegisterController>
-        {({ submit }) => <RegisterView submit={submit} />}
-        </RegisterController>
+      <RegisterController>
+        {({ submit }: { submit: any }) => <RegisterView submit={submit} />}
+      </RegisterController>
     );
   }
 }
